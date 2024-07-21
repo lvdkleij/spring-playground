b72c8638b3d6386a2cc1cbc18bf3a3cd4bc945bd
Lucas van der Kleij <kleijvander@gmail.com>
2024-07-21 09:58:02 +0200

    Creates multi-module setup for recipes api

---
+++ recipes-api/app/delivery/src/test/kotlin/nl/redbeard/recipesapi/RecipesApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package nl.redbeard.recipesapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RecipesApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
